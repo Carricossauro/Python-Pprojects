8a8bd4ca170b119c9c8ed5e0ba9acd18e41c2472
tiagocarrico27@gmail.com <tiagocarrico27@gmail.com>
2020-03-11 15:54:07 +0000

    API

--- PythonAPI/server.py
+++ PythonAPI/server.py
@@ -2,7 +2,7 @@
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 from sqlalchemy import create_engine
-from json import dumps
+from datetime import date
 
 db = create_engine('mysql://carricossauro:admin@localhost/PythonAPI')
 app = Flask(__name__)
@@ -34,6 +34,26 @@ def findUser():
         return "Error: No username field provided. Please specify a username."
     query = con.execute('SELECT username FROM PythonTable WHERE username="' + username + '"')
     return jsonify([dict(zip(tuple(query.keys()), i)) for i in query.cursor])
+
+
+@app.route('/users/add', methods=['GET', 'POST'])
+def addUser():
+    resp = 400
+    if 'username' in request.args and 'password' in request.args and 'email' in request.args:
+        try:
+            user = request.args['username']
+            passwd = request.args['password']
+            email = request.args['email']
+            todaysDate = date.today().strftime("%Y-%m-%d")
+            con.execute("INSERT INTO PythonTable "
+                        "(Username, password, email, CreationDay) VALUES "
+                        "('{}', '{}', '{}','{}')".format(user, passwd, email, todaysDate))
+            resp = 201
+        except Exception as error:
+            print(error)
+            resp = 409
+
+    return jsonify(resp)
 
 
 app.run()
